eabcb04b0cdee1044526586e3e9543da537279ae
vigih dev <vigihdev@gmail.com>
2026-01-18 02:35:16 +0700

    💥 2026-01-18 02:35:16

--- src/DTOs/Entities/BaseEntityDto.php
+++ src/DTOs/Entities/BaseEntityDto.php
@@ -7,6 +7,8 @@ namespace Vigihdev\WpCliModels\DTOs\Entities;
 abstract class BaseEntityDto
 {
     abstract public function toArray(): array;
+
     abstract public static function fromQuery(mixed $data): static;
+
     abstract public static function fromArray(array $data): static;
 }
